29242877f6b2608f07188c34c8d9a7835e4134a9
Cleidison Silva <cleidison.dev@gmail.com>
2024-01-03 22:43:26 -0300

    Desafio finalizado

---
+++ README.md
@@ -0,0 +1,8 @@
+## Desafio Conta Banco
+
+Desafio proposto pela plataforma [DIO](https://www.dio.me/) no curso de JAVA.
+
+## Descrição do desafio
+O usuário poderá fornecer os dados (nome, agência, conta e saldo) através do terminal. Após a inserção dos dados, o sistema exibirá uma mensagem ao usuário. Exemplo:
+
+![Alt text](/images/exemplo.png)
---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta");
+        int numeroConta = scanner.nextInt();
+
+        scanner.nextLine(); // Clean keyboard buffer
+        System.out.println("Por favor, digite seu nome");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá ".concat(nomeCliente)
+                + ", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia)
+                + ", conta ".concat(String.valueOf(numeroConta)) + " e seu saldo ".concat(String.valueOf(saldo))
+                + " já está disponível para saque.";
+
+        System.out.println();
+        System.out.println(mensagem);
+    }
+}
